57b4435c8e9a54a60757b0560a0b8190d6de1098
Aaditya210905 <aadityameghrajani05@gmail.com>
2025-09-25 14:30:19 +0530

    Add prediction sample script with mental health model

--- src/predict_sample.py
+++ src/predict_sample.py
@@ -12,7 +12,7 @@ le_target = joblib.load('models/label_encoder.pkl')
 sample_dict = {
     "Gender": "Male",
     "Occupation": "Student",
-    "self_employed": "NO",
+    "self_employed": "No",
     "family_history": "No",
     "Days_Indoors": "Go out Every day",
     "Growing_Stress": "No",
@@ -25,13 +25,37 @@ sample_dict = {
     "mental_health_interview": "No",
     "care_options": "Not sure"
 }
+
+sample_dict2 = {
+    "Gender": "Male",
+    "Occupation": "Student",
+    "self_employed": "No",
+    "family_history": "Yes",
+    "Days_Indoors": "More than 2 months",
+    "Growing_Stress": "Yes",
+    "Changes_Habits": "Yes",
+    "Mental_Health_History": "No",
+    "Mood_Swings": "High",
+    "Coping_Struggles": "Yes",
+    "Work_Interest": "Yes",
+    "Social_Weakness": "Yes",
+    "mental_health_interview": "Maybe",
+    "care_options": "Not sure"
+}
+
 # Convert to DataFrame
 sample_df = pd.DataFrame([sample_dict])
+sample_df2 = pd.DataFrame([sample_dict2])
 
 # Preprocess
 sample_df = build_preprocessor(sample_df=sample_df, is_sample=True,ordinal_encoder=ordinal_encoder, ohe=ohe, lda=lda)
+sample_df2 = build_preprocessor(sample_df=sample_df2, is_sample=True,ordinal_encoder=ordinal_encoder, ohe=ohe, lda=lda)
 
 # Predict
 pred = model.predict(sample_df)
 pred_label = le_target.inverse_transform(pred)
-print("Prediction:", pred_label[0])
+print("Prediction for sample 1:", pred_label[0])
+
+pred2 = model.predict(sample_df2)
+pred_label2 = le_target.inverse_transform(pred2)
+print("Prediction for sample 2:", pred_label2[0])
